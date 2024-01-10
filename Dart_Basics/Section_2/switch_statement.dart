0efb0cc5de33b6c1dce2e1df24391c36a450003f
Yum <125544953+YamnaIsrail@users.noreply.github.com>
2024-01-10 23:16:59 +0500

    Create switch_statement.dart

---
+++ Dart_Basics/Section_2/switch_statement.dart
@@ -0,0 +1,38 @@
+//IMPORTANT:
+/* in the parameter of switch you can either pass String value or int
+    and double and boolean are not allowed
+*/
+
+void main()
+{
+  var grade= "A";
+
+
+  switch(grade)
+      {
+    case 'A' :
+      print("Grade A");
+      break;
+
+    case 'B' :
+      print("Grade B");
+      break;
+
+    case 'C' :
+      print("Grade C");
+      break;
+
+    case 'D' :
+      print("Grade D");
+      break;
+
+    case 'E' :
+      print("Grade E");
+      break;
+
+    default:
+      print("Invalid");
+      break;
+
+  }
+}
