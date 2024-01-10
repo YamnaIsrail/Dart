5384cc1e7f2c4820131aaa1c187c0b441ddfb0bf
Yum <125544953+YamnaIsrail@users.noreply.github.com>
2024-01-10 23:22:00 +0500

    Create 2_for_loop.dart
    Implementation of for loop using integer and list of strings.

---
+++ Dart_Basics/Section_3/2_for_loop.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  for(int i=0; i<10; i++)
+    {
+      if(i%2== 0)
+        print(i);
+    }
+
+  List planets= ["mercury", "mars", "jupiter", "venus", "earth"];
+  for( String planet in planets )
+    {
+      print(planet);
+    }
+}
