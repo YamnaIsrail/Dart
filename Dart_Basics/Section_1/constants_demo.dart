371ba1945200638a8151c61cbf771db9b13d62dc
Yum <125544953+YamnaIsrail@users.noreply.github.com>
2024-01-10 20:02:01 +0500

    Introduce Dart file demonstrating the use of constants
    This commit adds a Dart file named 'constants_demo.dart' that illustrates the usage of constants in Dart. It covers both 'final' and 'const' keywords, explaining their characteristics and use cases. The examples include constant declarations in the main function and a class-level constant within a Circle class. This addition enhances the understanding of constants for learners in the Dart basics course.

---
+++ Dart_Basics/Section_1/constants_demo.dart
@@ -0,0 +1,16 @@
+// Dart Constants:
+// - 'final' variables can be set once and are initialized when accessed.
+// - 'const' variables are compile-time constants.
+
+void main() {
+  final String city = "Mansehra";
+  final String country = "Pakistan"; // Explicitly defining data type for clarity
+
+  const double pi = 3.14;
+  const double gravity = 9.8;
+}
+
+class Circle {
+  final String color = 'red';
+  static const double pi = 3.14; // 'static const' for class-level constant.
+}
