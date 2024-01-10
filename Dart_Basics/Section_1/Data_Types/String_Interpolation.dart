bf6e13dea1ecbcf1242c8160f0132db599b38589
Yum <125544953+YamnaIsrail@users.noreply.github.com>
2024-01-10 20:10:33 +0500

    Create String_Interpolation.dart

---
+++ Dart_Basics/Section_1/Data_Types/String_Interpolation.dart
@@ -0,0 +1,25 @@
+void main()
+{
+  //Literals
+  "name";
+  34;
+  true;
+  4.5;
+
+  //ways to def string literals in dart
+  String s1= "string";
+  String s2 = 'st2';
+  String s3= 'don\'t';
+  String s= " don't";
+  String s5= "In literary theory, a text is any object that can be read,"
+      "whether this object is a work of literature, a street sign," ;
+
+
+  //String Interpolation
+  String name = "henry";
+  print("My name is $name ");
+  print("name length ${name.length}");
+  int l=20;
+  int b=30;
+  print("$l + $b = ${l+b}");
+}
