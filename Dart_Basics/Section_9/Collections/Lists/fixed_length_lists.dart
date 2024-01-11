afdff52f7f4d11c9264710569b8052ab15fd0db0
Yum <125544953+YamnaIsrail@users.noreply.github.com>
2024-01-11 19:07:08 +0500

    Implementing fixed length lists

---
+++ Dart_Basics/Section_9/Collections/Lists/fixed_length_lists.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Fixed-length list
+  List<int> fixedList = List<int>.filled(3, 0); // Creates a fixed-length list of size 3 with initial value 0
+  
+  // Assigning values to the fixed-length list
+  fixedList[0] = 1;
+  fixedList[1] = 2;
+  fixedList[2] = 3;
+
+  print("Fixed-length list: $fixedList");
+}
